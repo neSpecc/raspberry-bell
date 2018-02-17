6ebe3130e300b92eb4b863df37a15bb77f55fe36
kontsarenko <kontsarenko@gmail.com>
2018-02-17 22:45:33 +0300

    18 pin to 7 pin

--- led.py
+++ led.py
@@ -2,9 +2,9 @@ import RPi.GPIO as GPIO
 import time
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
-GPIO.setup(18,GPIO.OUT)
+GPIO.setup(7,GPIO.OUT)
 print "LED on"
-GPIO.output(18,GPIO.HIGH)
+GPIO.output(7,GPIO.HIGH)
 time.sleep(1)
 print "LED off"
-GPIO.output(18,GPIO.LOW)
+GPIO.output(7,GPIO.LOW)
