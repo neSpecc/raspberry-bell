f9e3d90225524c4529f00307c84d321033697bf5
kontsarenko <kontsarenko@gmail.com>
2018-03-03 13:34:00 +0300

    fix

--- slack.sh
+++ slack.sh
@@ -12,21 +12,22 @@ SOCKET_DELAY = 1
 
 valet_slack_client = slackclient.SlackClient(config['token'])
 
-
-# TODO SLACK Specific
-
-
-
 def post_message(message, channel):
-    valet_slack_client.api_call('chat.postMessage', channel=channel,
-                          text=message, as_user=True)
+    valet_slack_client.api_call('chat.postMessage', channel=channel, text=message, as_user=True)
+
+def is_for_me(event):
+    """Know if the message is dedicated to me"""
+    type = event.get('type')
+        text = event.get('text')
+        channel = event.get('channel')
+        if valet_slack_mention in text.strip().split():
+            return True
 
 def say_hi():
     """Say Hi to a user by formatting their mention"""
     response_template = random.choice(['Ding!', '🛎'])
     ding.bing()
-    return response_template.format(mention=user_mention)
-
+    return response_template
 
 def is_hi(message):
     print (message.encode('UTF-8').lower().find(':moneybag: *Оплачена*'))
@@ -34,10 +35,8 @@ def is_hi(message):
 
 def handle_message(message, user, channel):
     if is_hi(message):
-        # user_mention = get_mention(user)
-        # os.system('servo.py')
-        # post_message(message=say_hi(), channel=channel)
         ding.bing()
+				post_message(message=say_hi(), channel=channel)
 
 # Bot Specific
 def run():
@@ -48,7 +47,6 @@ def run():
             if len(event_list) > 0:
                 for event in event_list:
                     print(event)
-                    # print (event)
                     handle_message(message=event.get('text'), user=event.get('user'), channel=event.get('channel'))
             time.sleep(SOCKET_DELAY)
     else:
