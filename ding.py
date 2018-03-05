4bb5e586e5b206ecbdfe9a3a798a1b2f6b2103ba
Philipp Kontsarenko <kontsarenko@gmail.com>
2018-03-05 15:47:49 +0300

    Update ding.py

--- ding.py
+++ ding.py
@@ -5,10 +5,10 @@ def bing():
   GPIO.setmode(GPIO.BCM)
   GPIO.setwarnings(False)
   GPIO.setup(18,GPIO.OUT)
-  print "Solenoid on"
+  print("Solenoid on")
   GPIO.output(18,1)
   time.sleep(0.1)
-  print "Solenoid off"
+  print("Solenoid off")
   GPIO.output(18,0)
   GPIO.cleanup()
 if __name__ == '__main__':
